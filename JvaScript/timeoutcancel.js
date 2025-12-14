73dde1a572658b2134eb7bc581746bd276f6628b
Pratibha Meher <meherprativa09@gmail.com>
2025-12-14 23:26:06 +0530

    Add files via upload

---
+++ JvaScript/timeoutcancel.js
@@ -0,0 +1,8 @@
+var cancellable = function(fn, args, t) {
+    const timeoutId = setTimeout(() => {
+        fn(...args);
+    } , t);
+    return function cancelFn(){
+        clearTimeout(timeoutId);
+    };
+};
